350eb6c1b3af12451374f8bbf25d76cc8e1b868b
SantiagoPapa14 <papa.santiago321@gmail.com>
2024-08-04 22:22:48 -0300

    Going to main page logs you out.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
+import {logout} from '@/lib/authLib';
 import { redirect } from "next/navigation";
 
 export default async function Home() {
+  await logout();
   redirect("/login");
   return (
     <></>
