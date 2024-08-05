f9ccfc26205b977f86f4cc7a35bea02f5d5da952
Santiago Papa <71857816+SantiagoPapa14@users.noreply.github.com>
2024-08-05 00:44:40 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,6 @@ import {logout} from '@/lib/authLib';
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  await logout();
   redirect("/login");
   return (
     <></>
